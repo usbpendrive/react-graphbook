51eaad4a8f4be1036ab05047d01300f8a0d54387
usbpendrive <ferry.fernando@gmail.com>
2020-11-26 15:56:20 +0700

    Add cors, compression, helmet for express server

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,26 @@
 import express from 'express';
 import path from 'path';
+import helmet from 'helmet';
+import cors from 'cors';
+import compress from 'compression';
 
 const root = path.join(__dirname, '../../');
 
 const app = express();
+
+app.use(helmet());
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "'unsafe-inline'"],
+        styleSrc: ["'self'", "'unsafe-inline'"],
+        imgSrc: ["'safe'", "data", "*.amazonaws.com"]
+    }
+}));
+app.use(helmet.referrerPolicy({ policy: 'same-origin' }));
+
+app.use(compress());
+app.use(cors());
 
 app.use('/', express.static(path.join(root, 'dist/client')));
 app.use('/uploads', express.static(path.join(root, 'uploads')));
