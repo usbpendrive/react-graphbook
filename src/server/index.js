ce53cd39e6b466642586d4a9c991527dbfc67e0a
usbpendrive <ferry.fernando@gmail.com>
2020-11-26 17:05:00 +0700

    Add graphql with apollo server middleware.

--- src/server/index.js
+++ src/server/index.js
@@ -4,26 +4,41 @@ import helmet from 'helmet';
 import cors from 'cors';
 import compress from 'compression';
 
+import services from './services';
+
 const root = path.join(__dirname, '../../');
 
 const app = express();
 
-app.use(helmet());
-app.use(helmet.contentSecurityPolicy({
-    directives: {
-        defaultSrc: ["'self'"],
-        scriptSrc: ["'self'", "'unsafe-inline'"],
-        styleSrc: ["'self'", "'unsafe-inline'"],
-        imgSrc: ["'safe'", "data", "*.amazonaws.com"]
-    }
-}));
-app.use(helmet.referrerPolicy({ policy: 'same-origin' }));
+if (process.env.NODE_ENV === 'production') {
+    app.use(helmet());
+    app.use(helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: ["'self'"],
+            scriptSrc: ["'self'", "'unsafe-inline'"],
+            styleSrc: ["'self'", "'unsafe-inline'"],
+            imgSrc: ["'safe'", "data", "*.amazonaws.com"]
+        }
+    }));
+    app.use(compress());
+    app.use(cors());
+}
 
-app.use(compress());
-app.use(cors());
+app.use(helmet.referrerPolicy({policy: 'same-origin'}));
 
 app.use('/', express.static(path.join(root, 'dist/client')));
 app.use('/uploads', express.static(path.join(root, 'uploads')));
+
+const servicesNames = Object.keys(services);
+
+for (let i = 0; i < servicesNames.length; i += 1) {
+    const name = servicesNames[i];
+    if (name === 'graphql') {
+        services[name].applyMiddleware({ app });
+    } else {
+        app.use(`/${name}`, services[name]);
+    }
+}
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(root, '/dist/client/index.html'));
