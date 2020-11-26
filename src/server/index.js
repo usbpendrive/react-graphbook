5e3e4f1205bc33c67558249a4c3f23fd98ab72d5
usbpendrive <ferry.fernando@gmail.com>
2020-11-26 15:51:42 +0700

    Serving static react build files.

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,15 @@
 import express from 'express';
+import path from 'path';
+
+const root = path.join(__dirname, '../../');
 
 const app = express();
 
-app.get('*', (req, res) => res.send('Hello Express!'));
+app.use('/', express.static(path.join(root, 'dist/client')));
+app.use('/uploads', express.static(path.join(root, 'uploads')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(root, '/dist/client/index.html'));
+});
+
 app.listen(8000, () => console.log('Listening on port 8000!'));
